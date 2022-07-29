78bc218f42096e159fecc16d165c9d73b29510d5
Martin-Ortiz <leo.martin178@gmail.com>
2022-07-29 16:05:57 -0500

    Curso de POO

---
+++ index.js
@@ -0,0 +1,17 @@
+/*Importación de clases*/
+import {Cliente} from './Cliente.js'
+import { Cuenta } from './Cuenta.js';
+import { CuentaAhorro } from './CuentaAhorro.js';
+import { CuentaCorriente } from './CuentaCorriente.js';
+
+
+const cliente = new Cliente('Leonardo','13804050','123224');
+const cliente2 = new Cliente('María','16979808','8989');
+
+const cuentaDeLeonardo = new CuentaCorriente(cliente, '1', '001');
+const cuentaDeMaria = new CuentaCorriente(cliente2,'2','002');
+
+const cuentaAhorroLeonardo = new CuentaAhorro(cliente, '9985', '001', 0);
+console.log(cuentaAhorroLeonardo);
+
+const cuentaSimple = new Cuenta(cliente, '098', '001');
